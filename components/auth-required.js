a3f336bd8b6ba21c8e4e06371d53c6401bf57d1e
Aria Stewart <aredridel@nbtsc.org>
2016-12-04 14:55:32 -0500

    Complete basic authentication

--- components/auth-required.js
+++ components/auth-required.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import Auth from './auth';
 import fetch from 'isomorphic-fetch'
+import slackFetch from '../lib/slack-fetch';
+import jwt from 'jwt-simple';
+import url from 'url';
+import cookie from 'cookie';
 
 export default (Component) => {
 	return class extends React.Component {
 		static async getInitialProps(ctx) { 
 			const { query, req } = ctx;
 			const superProps = typeof Component.getInitialProps == 'function' ? await Component.getInitialProps(ctx) : {}
-			const authProps = await this.getAuthProps(ctx);
-			return { ...superProps, ...authProps };
+			const auth = await this.getAuthProps(ctx);
+			return { ...superProps, auth };
 		}
 		
-		static async getAuthProps({req, query}) {
+		static async getAuthProps({req, res, query}) {
 			if (req) {
 				const self = `http://${req.headers.host}/auth`;
 				const client_id = process.env.SLACK_APP_CLIENT_ID;
@@ -22,24 +26,41 @@ export default (Component) => {
 					throw "SLACK_APP_CLIENT_ID and SLACK_APP_CLIENT_SECRET and JWT_KEY must be set to operate";
 				}
 				
+				const cookies = req.headers.cookie && cookie.parse(req.headers['cookie']);
+				
 				if (query.code) {
-					// Update login code here
-				} else if (/*has cookie*/true) {
-					// unpack cookie here
+          const body = await slackFetch('https://slack.com/api/oauth.access', {
+            client_id,
+            client_secret,
+            redirect_uri: `http://${req.headers.host}/auth`,
+            code: url.parse(req.url, true).query.code
+          });
+          if (!body.access_token) {
+            throw new Error("No access token granted");
+          }
+          
+          const tok = jwt.encode(Object.assign(body, { time: Date.now() }), KEY);
+          res.setHeader('Set-Cookie', cookie.serialize('auth', tok));
+          return {
+            loggedIn: true
+          };
+				} else if (cookies.auth && jwt.decode(cookies.auth, KEY)) {
+  				return {
+  				  loggedIn: true
+				  };
 				}
 	
 				return {
-					authurl: `https://slack.com/oauth/authorize?client_id=${client_id}&scope=files:read%20files:write:user&redirect_uri=${self}`,
-					loggedIn: true
+					authurl: `https://slack.com/oauth/authorize?client_id=${client_id}&scope=files:read%20files:write:user&redirect_uri=${self}`
 				}
 			}
 		}
 	
 		render() {
-			if (this.props.loggedIn) {
+			if (this.props.auth.loggedIn) {
 				return <Component {...this.props}/>
 			} else {
-				return <Auth authurl={this.props.authurl}/>
+				return <Auth authurl={this.props.auth.authurl}/>
 			}
 		}
 	}
