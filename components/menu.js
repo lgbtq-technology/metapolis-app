95f03dfb483f8beef0bf3e4c1abb34de9236258e
Aria Stewart <aredridel@nbtsc.org>
2016-12-04 14:55:44 -0500

    Add a main menu

---
+++ components/menu.js
@@ -0,0 +1,6 @@
+import Link from 'next/link';
+
+export default () => <div>
+  <Link href='/'>Home</Link>
+  <Link href='/purge'>Purge Private Files</Link>
+</div>;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Layout from '../components/layout';
+import Menu from '../components/menu';
 import AuthRequired from '../components/auth-required';
 
 export default AuthRequired(class extends React.Component {
 	render () {
 		return <Layout>
-			Hello
+			<Menu/>
 		</Layout>;
 	}
 })
