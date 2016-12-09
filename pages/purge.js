a6efd8511fdb853c978229966bc1dfa72fa4f30b
Aria Stewart <aredridel@nbtsc.org>
2016-12-09 01:16:17 -0500

    Purge file client side

--- pages/purge.js
+++ pages/purge.js
@@ -9,10 +9,15 @@ export default AuthRequired(class extends React.Component {
       
     }
   }
+
+  async purgeFiles(ev) {
+    ev.preventDefault()
+    const nfiles = await purgeFiles(this.props.auth.token)
+  }
   
   render() {
     return <Layout auth={this.props.auth}>
-      <form action='/purge' method='post'>
+      <form action='/purge' method='post' onSubmit={this.purgeFiles.bind(this)}>
         <button className='Button Button-dark' type='submit'>Purge private files</button>
       </form>
   
