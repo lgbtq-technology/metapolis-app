b77fbb91bff230367075a762b2e93855972f00f9
Aria Stewart <aredridel@nbtsc.org>
2016-12-11 01:29:47 -0500

    Rework auth (for now, dropping credential storage for any length of time)

--- components/auth-required.js
+++ components/auth-required.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import Auth from './auth';
-import fetch from 'isomorphic-fetch'
-import slackFetch from '../lib/slack-fetch';
-import jwt from 'jwt-simple';
-import url from 'url';
-import cookie from 'cookie';
-import getToken from '../lib/get-token';
+
+const client_id = '3275311328.17418201121';
 
 export default (Component) => {
   return class extends React.Component {
@@ -20,70 +16,18 @@ export default (Component) => {
     }
 
     static async getAuthProps({req, res, query}) {
-      if (req) {
-        const self = `http://${req.headers.host}/auth`;
-        const client_id = process.env.SLACK_APP_CLIENT_ID;
-        const client_secret = process.env.SLACK_APP_CLIENT_SECRET;
-        const KEY = process.env.JWT_KEY;
-
-        if (!client_id || !client_secret || !KEY) {
-          throw "SLACK_APP_CLIENT_ID and SLACK_APP_CLIENT_SECRET and JWT_KEY must be set to operate";
-        }
-
-        const token = getToken(req);
-
-        if (query.code) {
-          const body = await slackFetch('https://slack.com/api/oauth.access', {
-            client_id,
-            client_secret,
-            redirect_uri: `http://${req.headers.host}/auth`,
-            code: url.parse(req.url, true).query.code
-          });
-
-          if (!body.access_token) {
-            throw new Error("No access token granted");
-          }
-
-          const tok = jwt.encode(Object.assign(body, {
-            time: Date.now()
-          }), KEY);
-          res.setHeader('Set-Cookie', cookie.serialize('auth', tok));
-          return {
-            loggedIn: true,
-            token: body,
-            nextUrl: query.state
-          };
-        } else if (token) {
-          return {
-            loggedIn: true,
-            token
-          };
-        }
-
+      const self = `http://${req ? req.headers.host : window.location.host }/auth`;
+      if (typeof window != 'undefined' && window.auth) {
+        return window.auth
+      } else {
         return {
           authurl: `https://slack.com/oauth/authorize?client_id=${client_id}&scope=files:read%20files:write:user&redirect_uri=${self}&state=${req.url}`
         }
-      } else {
-        if (window.auth) {
-          return window.auth
-        }
       }
-    }
-
-    componentWillMount() {
-      if (this.props.auth.nextUrl) {
-        this.props.url.replaceTo(this.props.auth.nextUrl);
-      }
-    }
-
-    constructor(props) {
-      super(props);
-      if (typeof window != 'undefined')
-        window.auth = props.auth;
     }
 
     render() {
-      if (this.props.auth.loggedIn) {
+      if (this.props.auth.token) {
         return <Component {...this.props}/>
       } else {
         return <Auth authurl={this.props.auth.authurl}/>
--- pages/auth.js
+++ pages/auth.js
@@ -2,8 +2,59 @@ import React from 'react';
 import Layout from '../components/layout';
 import AuthRequired from '../components/auth-required';
 
-export default AuthRequired(class extends React.Component {
+export default class extends React.Component {
+    static async getInitialProps(ctx) {
+      const {query, req} = ctx;
+      const auth = await this.getAuthProps(ctx);
+      return {
+        auth
+      };
+    }
+
+    static async getAuthProps({req, res, query}) {
+      const self = `http://${req ? req.headers.host : window.location.host }/auth`;
+      if (query.code) {
+        const body = await fetch('http://localhost:3001/-/login', {
+          method: 'POST',
+          body: JSON.stringify({
+            ...query,
+            redirect_uri: self
+          }),
+          headers: {
+            "Content-Type": "application/json"
+          }
+        }).then(req => req.json());
+
+        if (!body.access_token) {
+          throw new Error("No access token granted");
+        }
+
+        return {
+          token: body,
+          nextUrl: query.state
+        };
+      } else if (typeof window != 'undefined' && window.auth) {
+        return window.auth
+      } else {
+        return {
+          authurl: `https://slack.com/oauth/authorize?client_id=${client_id}&scope=files:read%20files:write:user&redirect_uri=${self}&state=${req.url}`
+        }
+      }
+    }
+
+    componentWillMount() {
+      if (this.props.auth.nextUrl) {
+        this.props.url.replaceTo(this.props.auth.nextUrl);
+      }
+    }
+
+    constructor(props) {
+      super(props);
+      if (typeof window != 'undefined')
+        window.auth = props.auth;
+    }
+
 	render () {
 		return <Layout><div>Logging You In...</div></Layout>;
 	}
-})
+}
