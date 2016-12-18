3b1f03699c6ec1c89001b87c92774ba06a8fafde
Aria Stewart <aredridel@nbtsc.org>
2016-12-17 20:08:34 -0500

    Support token increases

--- components/auth-required.js
+++ components/auth-required.js
@@ -2,56 +2,84 @@ import React from 'react';
 import Auth from './auth';
 import cookie from 'cookie';
 
+const DEFAULT_PERMS = [
+  'files:read',
+  'files:write:user'
+];
+
 const client_id = '3275311328.17418201121';
 
-export default (Component) => {
+export default (perms, Component) => {
+  if (typeof perms == 'function') {
+    Component = perms;
+    perms = DEFAULT_PERMS;
+  }
+
   return class extends React.Component {
     static async getInitialProps(ctx) {
       const {query, req} = ctx;
       const superProps = typeof Component.getInitialProps == 'function' ? await Component.getInitialProps(ctx) : {}
       const auth = await this.getAuthProps(ctx);
+      const self = `http://${req ? req.headers.host : window.location.host }/auth`;
       return {
         ...superProps,
+        authurl: `https://slack.com/oauth/authorize?client_id=${client_id}&scope=${encodeURIComponent(perms.join(' '))}&redirect_uri=${self}&state=${ctx.pathname}`,
         auth
       };
     }
 
-    static async getAuthProps({req, res, query, pathname}) {
-      const self = `http://${req ? req.headers.host : window.location.host }/auth`;
+    static async getAuthProps({req, res, query}) {
       const session = req && req.headers.cookie && cookie.parse(req.headers.cookie).session
       if (req && session) {
         try {
           const token = await fetch(`http://localhost:3001/-/session/${session}`).then(res => res.json())
-          if (token) {
+          if (token && scopesMatch(token.scope, perms)) {
             return { token };
           }
         } catch (e) {
           console.warn(e)
         }
       }
-      
+
       if (typeof window != 'undefined' && window.auth) {
         return window.auth
-      } else {
-        return {
-          authurl: `https://slack.com/oauth/authorize?client_id=${client_id}&scope=files:read%20files:write:user%20channels:read&redirect_uri=${self}&state=${pathname}`
-        }
       }
     }
 
     constructor(props) {
       super(props)
-      if (props.auth.token) {
+      if (props.auth && props.auth.token) {
         global.auth = props.auth
       }
     }
 
     render() {
-      if (this.props.auth.token) {
+      if (this.props.auth && this.props.auth.token && scopesMatch(this.props.auth.token.scope, perms)) {
         return <Component {...this.props}/>
       } else {
-        return <Auth authurl={this.props.auth.authurl}/>
+        return <Auth authurl={this.props.authurl}/>
       }
     }
   }
+}
+
+function scopesMatch(user, required) {
+  if (typeof required == 'string') {
+    required = required.split(',');
+  }
+  if (typeof user == 'string') {
+    user = user.split(',');
+  }
+
+  console.warn(required, user);
+
+  const userSet = new Set(user);
+
+  for (let requirement of required) {
+    if (!userSet.has(requirement)) {
+      return false;
+    }
+  }
+
+  return true;
 }
