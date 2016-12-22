899dea0e9c4cc4827f51a0bd4515728a8d4fd23b
Aria Stewart <aredridel@nbtsc.org>
2016-12-22 01:40:50 -0500

    Add logout page

--- components/menu.js
+++ components/menu.js
@@ -4,4 +4,5 @@ export default () => <nav>
   <p><Link href='/'>Home</Link></p>
   <p><Link href='/purge'>Purge Private Files</Link></p>
   <p><Link href='/upload'>Upload File</Link></p>
+  <p><Link href='/logout'>Log Out</Link></p>
 </nav>;
---
+++ pages/logout.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import AuthRequired from '../components/auth-required';
+import Layout from '../components/layout';
+import purgeFiles from '../lib/purge-files';
+
+export default AuthRequired(class extends React.Component {
+
+  logout() {
+    document.cookie = "session=";
+    global.location.reload()
+  }
+
+  render() {
+    return <Layout auth={this.props.auth}>
+      <button onClick={() => this.logout()}>Log Out</button>
+    </Layout>
+  }
+})
