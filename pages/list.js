01d002793f4092824b93fb147a3b9126177da7e6
Aria Stewart <aredridel@nbtsc.org>
2017-01-03 00:51:41 -0500

    Add list files page

---
+++ pages/list.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Link from 'next/link';
+import Layout from '../components/layout';
+import AuthRequired from '../components/auth-required';
+import fetch from 'isomorphic-fetch';
+import url from 'url';
+import config from '../config';
+
+export default AuthRequired(class extends React.Component {
+  static async getInitialProps({auth}) {
+    const result = await fetch(url.resolve(config.api, `/-/files.list?user=${auth.token.user_id}`), {
+      headers: {
+        'Authorization': `Bearer ${auth.sid}`
+      }
+    });
+
+    return await result.json();
+  }
+
+  // {"user":"U038225J3","team":"T0383959N","file":"9B9826D7","name":"test","type":"image/png","unfurl":true,"path":"/-/files/T0383959N/U038225J3/9B9826D7.png"}
+
+  render() {
+    console.warn(this.props);
+    return <Layout auth={this.props.auth}>
+      <h2>Your files</h2>
+      {
+        this.props.files.map(file => <div key={file.file}><Link href={`/file?f=${file.team}/${file.user}/${file.file}`}>{ file.title || file.name }</Link></div>)
+      }
+    </Layout>;
+  }
+})
