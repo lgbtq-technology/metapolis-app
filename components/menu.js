c8b8ca216fa5ed2fb74d3fde42e07b92ea29f245
Aria Stewart <aredridel@nbtsc.org>
2017-01-03 00:54:00 -0500

    add link to new page

--- components/menu.js
+++ components/menu.js
@@ -4,5 +4,6 @@ export default () => <nav>
   <p><Link href='/'>Home</Link></p>
   <p><Link href='/purge'>Purge Private Files</Link></p>
   <p><Link href='/upload'>Upload File</Link></p>
+  <p><Link href='/list'>List Your Files</Link></p>
   <p><Link href='/logout'>Log Out</Link></p>
 </nav>;
