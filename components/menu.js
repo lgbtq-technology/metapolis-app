3da87623bc4f2cc7d67120cfa27fa1b96b3d644d
logan crossan <crossanlogan@gmail.com>
2017-05-11 13:56:49 -0400

    fixed deprecated <Link> warning

--- components/menu.js
+++ components/menu.js
@@ -1,9 +1,11 @@
 import Link from 'next/link';
 
-export default () => <nav>
-  <p><Link href='/'>Home</Link></p>
-  <p><Link href='/purge'>Purge Private Files</Link></p>
-  <p><Link href='/upload'>Upload File</Link></p>
-  <p><Link href='/list'>List Your Files</Link></p>
-  <p><Link href='/logout'>Log Out</Link></p>
-</nav>;
+export default () => (
+  <nav>
+    <p><Link><a href='/'>Home</a></Link></p>
+    <p><Link><a href='/purge'>Purge Private Files</a></Link></p>
+    <p><Link><a href='/upload'>Upload File</a></Link></p>
+    <p><Link><a href='/list'>List Your Files</a></Link></p>
+    <p><Link><a href='/logout'>Log Out</a></Link></p>
+  </nav>
+);
