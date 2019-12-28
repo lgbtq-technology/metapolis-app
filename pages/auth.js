7b39f0df8bc005f6e41959ff45e85735fda4038b
Aria Stewart <aredridel@dinhe.net>
2019-12-27 19:38:11 -0500

    format

--- pages/auth.js
+++ pages/auth.js
@@ -8,66 +8,66 @@ import config from '../config';
 
 export default class extends React.Component {
     static async getInitialProps(ctx) {
-      const {query, req} = ctx;
-      const auth = await this.getAuthProps(ctx);
-      return {
-        auth
-      };
+	const {query, req} = ctx;
+	const auth = await this.getAuthProps(ctx);
+	return {
+	    auth
+	};
     }
 
     static async getAuthProps({req, res, query}) {
-      const self = url.resolve(config.self, '/auth');
-      if (query.code) {
-        const body = await fetch(url.resolve(config.api, '/-/login'), {
-          method: 'POST',
-          body: JSON.stringify({
-            ...query,
-            redirect_uri: self
-          }),
-          headers: {
-            "Content-Type": "application/json"
-          }
-        }).then(req => req.json());
+	const self = url.resolve(config.self, '/auth');
+	if (query.code) {
+	    const body = await fetch(url.resolve(config.api, '/-/login'), {
+		method: 'POST',
+		body: JSON.stringify({
+		    ...query,
+		    redirect_uri: self
+		}),
+		headers: {
+		    "Content-Type": "application/json"
+		}
+	    }).then(req => req.json());
 
-        if (!body.token) {
-          throw new Error("No access token granted");
-        }
+	    if (!body.token) {
+		throw new Error("No access token granted");
+	    }
 
-	if (body.sid) {
-	    const expires = new Date();
-	    expires.setDate(expires.getDate() + 30);
-	    const sessionCookie = cookie.serialize('session', body.sid, { expires });
-	    if (res) {
-		res.setHeader('Set-Cookie', sessionCookie);
-	    } else {
-		document.cookie = sessionCookie;
+	    if (body.sid) {
+		const expires = new Date();
+		expires.setDate(expires.getDate() + 30);
+		const sessionCookie = cookie.serialize('session', body.sid, { expires });
+		if (res) {
+		    res.setHeader('Set-Cookie', sessionCookie);
+		} else {
+		    document.cookie = sessionCookie;
+		}
+	    }
+
+	    return {
+		...body,
+		nextUrl: query.state
+	    };
+	} else if (typeof window != 'undefined' && window.auth) {
+	    return window.auth
+	} else {
+	    const client_id = process.env.SLACK_APP_CLIENT_ID;
+	    return {
+		authurl: `https://slack.com/oauth/authorize?client_id=${client_id}&scope=files:read%20files:write:user&redirect_uri=${self}&state=${req.url}`
 	    }
 	}
-
-        return {
-	  ...body,
-          nextUrl: query.state
-        };
-      } else if (typeof window != 'undefined' && window.auth) {
-        return window.auth
-      } else {
-	const client_id = process.env.SLACK_APP_CLIENT_ID;
-        return {
-          authurl: `https://slack.com/oauth/authorize?client_id=${client_id}&scope=files:read%20files:write:user&redirect_uri=${self}&state=${req.url}`
-        }
-      }
     }
 
     componentWillMount() {
-      if (this.props.auth.nextUrl) {
-        this.props.url.replace(this.props.auth.nextUrl);
-      }
+	if (this.props.auth.nextUrl) {
+	    this.props.url.replace(this.props.auth.nextUrl);
+	}
     }
 
     constructor(props) {
-      super(props);
-      if (typeof window != 'undefined')
-        window.auth = props.auth;
+	super(props);
+	if (typeof window != 'undefined')
+	    window.auth = props.auth;
     }
 
     render () {
